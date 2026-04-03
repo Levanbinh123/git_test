12239fb2714931cec3304d966961ab9e0166ed24
Levanbinh123 <levanbin1712@gmail.com>
2026-04-03 15:36:44 +0700

    first commit

---
+++ src/main/java/com/example/selenium/framework/SeleniumFramework.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.selenium.framework;
+
+/**
+ *
+ * @author young
+ */
+public class SeleniumFramework {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
+import utils.DriverFactory;
+import org.testng.Assert;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = DriverFactory.createDriver();
+        driver.get("https://www.google.com");
+    }
+
+    @Test
+    public void testTitle() {
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Google"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
