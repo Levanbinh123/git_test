09b4f21853aa1ae589f0a561535cae371fe63a19
Levanbinh123 <levanbin1712@gmail.com>
2026-04-03 16:04:58 +0700

    d

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -10,6 +10,12 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.*;
 import utils.DriverFactory;
 import org.testng.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+import org.testng.Assert;
+import utils.DriverFactory;
+import utils.ConfigUtil;
 
 public class LoginTest {
 
@@ -19,17 +25,38 @@ public class LoginTest {
 @Parameters("browser")
 public void setup(@Optional("chrome") String browser) {
     driver = DriverFactory.createDriver(browser);
-    driver.get("https://www.google.com"); // THÊM DÒNG NÀY
+    driver.get(ConfigUtil.getBaseUrl()); // THÊM DÒNG NÀY
 }
-    @Test
-    public void testTitle() {
-        String title = driver.getTitle();
-        Assert.assertTrue(title.contains("Google"));
-       
+ 
+    
+      @Test
+    public void testLoginSuccess() {
+
+        // Lấy từ GitHub Secrets (CI) hoặc fallback local
+        String username = ConfigUtil.getUsername();
+        String password = ConfigUtil.getPassword();
+
+        // Nhập username
+        driver.findElement(By.id("user-name")).sendKeys(username);
+
+        // Nhập password
+        driver.findElement(By.id("password")).sendKeys(password);
+
+        // Click login
+        driver.findElement(By.id("login-button")).click();
+
+        // Kiểm tra login thành công
+        String currentUrl = driver.getCurrentUrl();
+
+        Assert.assertTrue(currentUrl.contains("inventory"),
+                "Login failed!");
     }
 
-    @AfterMethod
+
+      @AfterMethod
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
